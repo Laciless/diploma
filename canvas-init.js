01a5afad594b47791b3fac9b849719a11e14d6ef
Anton Bohomaz <aboho@softserveinc.com>
2019-06-04 21:30:47 +0300

    font fix.

--- canvas-init.js
+++ canvas-init.js
@@ -5,8 +5,13 @@ const canvasHeight = Math.floor(window.innerHeight * 0.65);
 fractalCanvas.height = posterCanvas.height = canvasHeight;
 fractalCanvas.width = posterCanvas.width = canvasHeight / 1.4;
 
-let FabricCanvas = new fabric.Canvas('posterCanvas');
-
-FabricCanvas.setBackgroundColor('white', () => {
-    FabricCanvas.renderAll();
+const fonts = ['Roboto', 'Pacifico', 'Expletus Sans', 'Dancing Script', 'Shadows Into Light'];
+const fontfaceObservables = fonts.map(font => new FontFaceObserver(font));
+const observe = fontfaceObservables.map(obs => obs.load());
+let FabricCanvas;
+Promise.all(observe).then(() => {
+    FabricCanvas = new fabric.Canvas('posterCanvas');
+    FabricCanvas.setBackgroundColor('white', () => {
+        FabricCanvas.renderAll();
+    })
 })
